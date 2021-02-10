7e94ee80919c197fa97a0d06216546998e662783
Mantosh Sah <59989979+mantu1738@users.noreply.github.com>
2021-02-10 13:48:21 +0530

    first ever transitions

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import highway from '@dogstudio/highway';
+import Highway from '@dogstudio/highway';
+import Fade from './transition';
+
+const H = new Highway.Core({
+    transitions: {
+        default:Fade
+    }
+});
